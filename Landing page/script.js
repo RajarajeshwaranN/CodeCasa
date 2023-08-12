d981bd4757c9796eb4a91e88db5a8b74f1ad917d
RajarajeshwaranN <122969400+RajarajeshwaranN@users.noreply.github.com>
2023-08-12 16:03:18 +0530

    Add files via upload

---
+++ Landing page/script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    let element = document.body;
+    element.classList.toggle("dark");
+    let btn=document.getElementsByClassName('btn')[0];
+    btn.classList.toggle('dark')
+}
+
+function button(){
+    alert("Ordered!!!!!");
+}
